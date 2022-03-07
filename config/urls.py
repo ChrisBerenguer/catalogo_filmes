efbc33c3442361234023114841f59cdbda7d4bca
ChrisBerenguer <chrisberenguer300@gmail.com>
2022-03-06 21:26:37 -0300

    Projeto Semana 5

--- config/urls.py
+++ config/urls.py
@@ -13,12 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, re_path, include, register_converter
+from django.conf.urls.static import static
 
 #app_name: 'seriados'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('seriados/', include('seriados.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
