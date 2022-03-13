e9091596e3f87a02f7cfd39db67a2dae9e16d0dc
ChrisBerenguer <chrisberenguer300@gmail.com>
2022-03-13 18:47:29 -0300

    Projeto Django 13-03-2022

--- seriados/admin.py
+++ seriados/admin.py
@@ -1,12 +1,10 @@
 from atexit import register
+
 from django.contrib import admin
 
 from . import models
+from .models import Serie
 
 admin.site.register(models.Serie)
 admin.site.register(models.Temporada)
 admin.site.register(models.Episodio)
-
-
-
-# Register your models here.
--- seriados/forms.py
+++ seriados/forms.py
@@ -1,12 +1,12 @@
-from dataclasses import fields
 from django import forms
 
-from .models import Serie, Temporada, Episodio
+from .models import Episodio, Serie, Temporada
+
 
 class SerieForm(forms.Form):
     nome = forms.CharField(label="Nome da Série", max_length=70)
-    
-    
+
+
 class TemporadaForm(forms.ModelForm):
     class Meta:
         model = Temporada
